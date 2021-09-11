ae00cebf9e2dbe1abc6332fc2cd02ec3ebf2ff7d
maiko-m <mn.6182.nm@gmail.com>
2021-09-11 14:11:06 +0900

    :sparkles: init

---
+++ combines/Screen/Feed/Builder/FeedBuilder.swift
@@ -0,0 +1,15 @@
+//
+//  FeedBuilder.swift
+//  combines
+//
+//  Created by maiko morisaki on 2021/09/11.
+//
+
+import Foundation
+
+class FeedBuilder {
+    func build() -> FeedViewController {
+        let controller = FeedViewController()
+        return controller
+    }
+}
---
+++ combines/Screen/Feed/View/FeedViewController.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewController.swift
+//  combines
+//
+//  Created by maiko morisaki on 2021/09/11.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+}
